8d875bec30a3d27396c4d06fb7d0e8e6e939a9b8
lflangis <lflangis@ferlab.bio>
2023-05-11 15:12:40 -0400

    feat(entity): SJIP-423 add table extra and redirect link

--- src/views/ParticipantEntity/BiospecimenTable/index.tsx
+++ src/views/ParticipantEntity/BiospecimenTable/index.tsx
@@ -1,11 +1,17 @@
 import intl from 'react-intl-universal';
 import { useDispatch } from 'react-redux';
-import { EntityTable } from '@ferlab/ui/core/pages/EntityPage';
+import { addQuery } from '@ferlab/ui/core/components/QueryBuilder/utils/useQueryBuilderState';
+import { generateQuery, generateValueFilter } from '@ferlab/ui/core/data/sqon/utils';
+import { EntityTable, EntityTableRedirectLink } from '@ferlab/ui/core/pages/EntityPage';
+import { INDEXES } from 'graphql/constants';
 import { IParticipantEntity } from 'graphql/participants/models';
+import { DATA_EXPLORATION_QB_ID } from 'views/DataExploration/utils/constant';
 
 import DownloadDataButton from 'components/Biospecimens/DownloadDataButton';
+import ExternalLinkIcon from 'components/Icons/ExternalLinkIcon';
 import { useUser } from 'store/user';
 import { updateUserConfig } from 'store/user/thunks';
+import { STATIC_ROUTES } from 'utils/routes';
 
 import { SectionId } from '../utils/anchorLinks';
 import { getBiospecimenColumns, getBiospecimensFromParticipant } from '../utils/biospecimens';
@@ -27,6 +33,30 @@ const BiospecimenTable = ({ participant, loading }: OwnProps) => {
       loading={loading}
       data={biospecimens}
       title={intl.get('entities.biospecimen.biospecimen')}
+      titleExtra={[
+        <EntityTableRedirectLink
+          key="1"
+          to={STATIC_ROUTES.DATA_EXPLORATION_BIOSPECIMENS}
+          icon={<ExternalLinkIcon width={14} />}
+          onClick={() =>
+            addQuery({
+              queryBuilderId: DATA_EXPLORATION_QB_ID,
+              query: generateQuery({
+                newFilters: [
+                  generateValueFilter({
+                    field: 'participant_id',
+                    value: participant ? [participant.participant_id] : [],
+                    index: INDEXES.PARTICIPANT,
+                  }),
+                ],
+              }),
+              setAsActive: true,
+            })
+          }
+        >
+          {intl.get('global.viewInDataExploration')}
+        </EntityTableRedirectLink>,
+      ]}
       header={intl.get('entities.biospecimen.biospecimen')}
       columns={getBiospecimenColumns()}
       initialColumnState={userInfo?.config.participants?.tables?.biospecimens?.columns}
