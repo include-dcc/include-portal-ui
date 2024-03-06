2f1b6bf2676513160846c3fa68ad5a3b92c4170d
GaelleA <galtefrohne@ferlab.bio>
2024-03-06 15:59:14 -0500

    feat(study): SJIP-710 add data access section

--- src/graphql/studies/models.ts
+++ src/graphql/studies/models.ts
@@ -4,6 +4,27 @@ export interface IStudyResultTree {
   study: ArrangerResultsTree<IStudyEntity>;
 }
 
+export interface IStudyDataset {
+  id: string;
+  dataset_id?: string;
+  dataset_name?: string;
+  date_collection_start_year?: string;
+  date_collection_end_year?: string;
+  data_category?: string;
+  data_type?: string;
+  expected_data_categories?: string[];
+  experimental_strategy?: string;
+  experimental_platform?: string;
+  publication?: string;
+  access_limitation?: string[];
+  access_requirement?: string[];
+  repository?: string;
+  repository_url?: string;
+  participant_count?: string;
+  biospecimen_count?: string;
+  file_count?: string;
+}
+
 export interface IStudyEntity {
   id: string;
   study_id: string;
@@ -18,6 +39,7 @@ export interface IStudyEntity {
   part_lifespan_stage?: string[];
   data_category: string[];
   data_source?: string[];
+  dataset?: ArrangerResultsTree<IStudyDataset>;
   date_collection_start_year?: string;
   date_collection_end_year?: string;
   description?: string;
---
+++ src/views/StudyEntity/utils/dataAccess.tsx
@@ -0,0 +1,63 @@
+import { ReactNode } from 'react';
+import intl from 'react-intl-universal';
+import { TABLE_EMPTY_PLACE_HOLDER } from '@ferlab/ui/core/common/constants';
+import { IArrangerResultsTree } from '@ferlab/ui/core/graphql/types';
+import { IEntityDescriptionsItem } from '@ferlab/ui/core/pages/EntityPage';
+import { Typography } from 'antd';
+import { IStudyDataset, IStudyEntity } from 'graphql/studies/models';
+
+const { Text } = Typography;
+
+const getFlatDataset = (dataset?: IArrangerResultsTree<IStudyDataset>) => {
+  if (!dataset?.hits?.edges?.length) return undefined;
+
+  const accessLimitation: string[] = [];
+  const accessRequirement: string[] = [];
+  dataset.hits.edges.forEach((edge) => {
+    if (edge.node.access_limitation && edge.node.access_limitation.length > 0)
+      accessLimitation.push(...edge.node.access_limitation);
+    if (edge.node.access_requirement && edge.node.access_requirement.length > 0)
+      accessRequirement.push(...edge.node.access_requirement);
+  });
+
+  return {
+    accessLimitation: new Set(accessLimitation),
+    accessRequirement: new Set(accessRequirement),
+  };
+};
+
+const renderAccess = (data?: Set<string>): ReactNode => {
+  if (!data) return TABLE_EMPTY_PLACE_HOLDER;
+  const formattedData = Array.from(data).map((value: string) => <div>{value}</div>);
+  return <>{formattedData}</>;
+};
+
+const getDataAccessDescriptions = (study?: IStudyEntity): IEntityDescriptionsItem[] => {
+  const flatDataset = getFlatDataset(study?.dataset);
+
+  return [
+    {
+      label: intl.get('entities.study.access_limitation'),
+      value: renderAccess(flatDataset?.accessLimitation),
+    },
+    {
+      label: intl.get('entities.study.access_requirement'),
+      value: renderAccess(flatDataset?.accessRequirement),
+    },
+    {
+      label: intl.get('entities.study.study_contact'),
+      value:
+        study?.contact_name || study?.institution || study?.contact_email ? (
+          <>
+            {study.contact_name && <Text>{study.contact_name};</Text>}
+            {study.institution && <Text>{study.institution};</Text>}
+            {study.contact_email && <Text>{study.contact_email}</Text>}
+          </>
+        ) : (
+          TABLE_EMPTY_PLACE_HOLDER
+        ),
+    },
+  ];
+};
+
+export default getDataAccessDescriptions;
