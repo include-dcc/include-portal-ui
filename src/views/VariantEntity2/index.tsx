e66b68fa6743ce91c238a3ec79e53ba85500b9c0
AltefrohneGaelle <133775440+AltefrohneGaelle@users.noreply.github.com>
2024-03-19 15:41:50 -0400

    feat(variant): SJIP-739 add consequence section (#506)

--- src/views/VariantEntity2/index.tsx
+++ src/views/VariantEntity2/index.tsx
@@ -1,7 +1,10 @@
 import intl from 'react-intl-universal';
 import { useParams } from 'react-router-dom';
 import { IAnchorLink } from '@ferlab/ui/core/components/AnchorMenu';
+import { NO_GENE } from '@ferlab/ui/core/components/Consequences/Cell';
+import { hydrateResults } from '@ferlab/ui/core/graphql/utils';
 import EntityPageWrapper, { EntityTitle } from '@ferlab/ui/core/pages/EntityPage';
+import EntityNestedTable from '@ferlab/ui/core/pages/EntityPage/EntityNestedTable';
 import EntityVariantSummary from '@ferlab/ui/core/pages/EntityPage/EntityVariantSummary';
 import { Tag } from 'antd';
 
@@ -9,6 +12,7 @@ import LineStyleIcon from 'components/Icons/LineStyleIcon';
 
 import { useVariantEntity } from '../../graphql/variants/actions';
 
+import { expandedRowRender, getColumn } from './utils/consequence';
 import { getSummaryItems } from './utils/summary';
 
 import styles from './index.module.scss';
@@ -46,6 +50,10 @@ export default function VariantEntity() {
     values: locus ? [locus] : [],
   });
 
+  const geneSymbolOfPicked = data?.genes?.hits?.edges?.find((e) =>
+    (e.node.consequences || [])?.hits?.edges?.some((e) => e.node?.picked),
+  )?.node?.symbol;
+
   return (
     <EntityPageWrapper
       pageId="variant-entity-page"
@@ -73,6 +81,19 @@ export default function VariantEntity() {
           id={SectionId.SUMMARY}
           loading={loading}
           data={getSummaryItems(data)}
+          noDataLabel={intl.get('no.data.available')}
+        />
+
+        <EntityNestedTable
+          columns={getColumn(geneSymbolOfPicked)}
+          data={hydrateResults(data?.genes?.hits?.edges || []).filter(
+            (gene) => gene.symbol !== NO_GENE,
+          )}
+          expandedRowRender={expandedRowRender}
+          id={SectionId.CONSEQUENCE}
+          loading={loading}
+          title={intl.get('screen.variants.consequences.consequence')}
+          header={intl.get('screen.variants.consequences.transcripts')}
           noDataLabel={intl.get('no.data.available')}
         />
       </>
---
+++ src/views/VariantEntity2/utils/consequence.tsx
@@ -0,0 +1,273 @@
+import { ReactNode } from 'react';
+import intl from 'react-intl-universal';
+import { CheckCircleFilled } from '@ant-design/icons';
+import { TABLE_EMPTY_PLACE_HOLDER } from '@ferlab/ui/core/common/constants';
+import { pickImpactBadge } from '@ferlab/ui/core/components/Consequences/Cell';
+import ExternalLink from '@ferlab/ui/core/components/ExternalLink';
+import CanonicalIcon from '@ferlab/ui/core/components/Icons/CanonicalIcon';
+import { ProColumnType } from '@ferlab/ui/core/components/ProTable/types';
+import ExpandableCell from '@ferlab/ui/core/components/tables/ExpandableCell';
+import { hydrateResults } from '@ferlab/ui/core/graphql/utils';
+import StackLayout from '@ferlab/ui/core/layout/StackLayout';
+import {
+  getLongPredictionLabelIfKnown,
+  INDEX_IMPACT_PREDICTION_FIELD,
+  INDEX_IMPACT_PREDICTION_SHORT_LABEL,
+  INDEX_IMPACT_SCORE,
+} from '@ferlab/ui/core/pages/EntityPage/utils/consequences';
+import { toExponentialNotation } from '@ferlab/ui/core/utils/numberUtils';
+import { removeUnderscoreAndCapitalize } from '@ferlab/ui/core/utils/stringUtils';
+import { Space, Table, Tag, Tooltip, Typography } from 'antd';
+import { ColumnType } from 'antd/lib/table';
+import {
+  IConsequenceEntity,
+  IConservationsEntity,
+  IGeneEntity,
+  IPredictionEntity,
+} from 'graphql/variants/models';
+import { capitalize } from 'lodash';
+
+import { getEntityConsequenceDictionary } from 'utils/translation';
+
+import style from '../index.module.scss';
+
+const { Text } = Typography;
+
+export const getColumn = (geneSymbolOfPicked?: string): ProColumnType[] => [
+  {
+    key: 'symbol',
+    title: intl.get('screen.variants.consequences.gene'),
+    render: (gene: IGeneEntity) =>
+      gene.symbol ? (
+        <>
+          <ExternalLink href={`https://www.omim.org/entry/${gene.omim_gene_id}`}>
+            {gene.symbol}
+          </ExternalLink>
+          {gene.symbol === geneSymbolOfPicked && (
+            <Tooltip title={intl.get('screen.variants.consequences.pickedTooltip')}>
+              <CheckCircleFilled className={style.pickedIcon} />
+            </Tooltip>
+          )}
+        </>
+      ) : (
+        TABLE_EMPTY_PLACE_HOLDER
+      ),
+  },
+  {
+    key: 'biotype',
+    dataIndex: 'biotype',
+    title: intl.get('screen.variants.consequences.geneType'),
+    render: (biotype: string) =>
+      biotype ? removeUnderscoreAndCapitalize(biotype) : TABLE_EMPTY_PLACE_HOLDER,
+  },
+  {
+    key: 'pli',
+    dataIndex: 'gnomad',
+    title: intl.get('screen.variants.consequences.gnomad.pli'),
+    render: (gnomad: { pli: number }) =>
+      gnomad?.pli ? toExponentialNotation(gnomad.pli) : TABLE_EMPTY_PLACE_HOLDER,
+  },
+  {
+    key: 'loeuf',
+    dataIndex: 'gnomad',
+    title: intl.get('screen.variants.consequences.gnomad.loeuf'),
+    render: (gnomad: { loeuf: string }) => gnomad?.loeuf || TABLE_EMPTY_PLACE_HOLDER,
+  },
+  {
+    key: 'spliceai',
+    dataIndex: 'spliceai',
+    title: intl.get('screen.variants.consequences.spliceAi'),
+    render: (spliceAi: { ds: number; type: string[] }) =>
+      spliceAi?.ds ? (
+        <>
+          <span className={style.spliceAi}>{spliceAi.ds}</span>
+          {spliceAi.type.map((t: string, index: number) => (
+            <Tooltip title={intl.get(`screen.variants.summary.details.spliceAiType.${t}`)}>
+              <Tag key={index}>{t}</Tag>
+            </Tooltip>
+          ))}
+        </>
+      ) : (
+        TABLE_EMPTY_PLACE_HOLDER
+      ),
+  },
+];
+
+export const getPredictionScore = (
+  predictions: IPredictionEntity,
+  dictionary: {
+    sift: string;
+    fathmm: string;
+    caddRaw: string;
+    caddPhred: string;
+    dann: string;
+    lrt: string;
+    revel: string;
+    polyphen2: string;
+  },
+): (string | number | null)[][] =>
+  [
+    [dictionary.sift, predictions?.sift_pred, predictions?.sift_score],
+    [dictionary.fathmm, predictions?.fathmm_pred, predictions?.fathmm_score],
+    [dictionary.caddRaw, null, predictions?.cadd_score],
+    [dictionary.caddPhred, null, predictions?.cadd_phred],
+    [dictionary.dann, null, predictions?.dann_score],
+    [dictionary.lrt, predictions?.lrt_pred, predictions?.lrt_score],
+    [dictionary.revel, null, predictions?.revel_score],
+    [dictionary.polyphen2, predictions?.polyphen2_hvar_pred, predictions?.polyphen2_hvar_score],
+  ].filter(([, , score]) => score || score === 0);
+
+export const getExpandedColumns = (): ColumnType<any>[] => [
+  {
+    key: 'aa_change',
+    dataIndex: 'aa_change',
+    title: (
+      <Tooltip title={intl.get('screen.variants.consequences.aaColumnTooltip')}>
+        <Text className={style.tooltip}>{intl.get('screen.variants.consequences.aaColumn')}</Text>
+      </Tooltip>
+    ),
+    render: (aa_change: string) => aa_change || TABLE_EMPTY_PLACE_HOLDER,
+  },
+  {
+    key: 'consequence',
+    title: intl.get('screen.variants.consequences.consequence'),
+    render: (consequence: IConsequenceEntity) => {
+      if (!consequence.consequence?.length) return TABLE_EMPTY_PLACE_HOLDER;
+      return (
+        <>
+          <Tooltip
+            title={intl.get(`screen.variants.consequences.impactTooltip.${consequence.vep_impact}`)}
+          >
+            <Text>{pickImpactBadge(consequence.vep_impact)}</Text>
+          </Tooltip>
+          <Text className={style.consequence}>
+            {removeUnderscoreAndCapitalize(consequence.consequence[0])}
+          </Text>
+        </>
+      );
+    },
+  },
+  {
+    key: 'coding_dna_change',
+    dataIndex: 'coding_dna_change',
+    title: intl.get('screen.variants.consequences.cdnaChangeColumn'),
+    render: (coding_dna_change: string) => coding_dna_change || TABLE_EMPTY_PLACE_HOLDER,
+  },
+  {
+    key: 'predictions',
+    dataIndex: 'predictions',
+    title: intl.get('screen.variants.consequences.predictions.prediction'),
+    render: (predictions: IPredictionEntity) => {
+      const impact = getPredictionScore(predictions, getEntityConsequenceDictionary().predictions);
+      if (!impact?.length) return TABLE_EMPTY_PLACE_HOLDER;
+      return (
+        <ExpandableCell
+          dataSource={impact}
+          dictionnary={{
+            'see.less': intl.get('see.less'),
+            'see.more': intl.get('see.more'),
+          }}
+          nOfElementsWhenCollapsed={2}
+          renderItem={(item: any, id) => {
+            const predictionField = item[INDEX_IMPACT_PREDICTION_FIELD];
+            const score = item[INDEX_IMPACT_SCORE];
+            const predictionShortLabel = item[INDEX_IMPACT_PREDICTION_SHORT_LABEL];
+            const predictionLongLabel = getLongPredictionLabelIfKnown(
+              predictionField,
+              predictionShortLabel,
+            );
+            const label = predictionLongLabel || predictionShortLabel;
+            const description = label ? `${capitalize(label)} (${score})` : score;
+
+            return (
+              <StackLayout horizontal key={id}>
+                <Text className={style.predictionLabel}>
+                  {intl.get(
+                    `screen.variants.summary.details.${
+                      predictionField[0].toLowerCase() + predictionField.slice(1)
+                    }`,
+                  )}
+                  :
+                </Text>
+                <Text>{description}</Text>
+              </StackLayout>
+            );
+          }}
+        />
+      );
+    },
+  },
+  {
+    key: 'conservations',
+    dataIndex: 'conservations',
+    title: intl.get('screen.variants.consequences.conservation'),
+    render: (conservations: IConservationsEntity) =>
+      conservations?.phyloP17way_primate ? (
+        <StackLayout horizontal>
+          <Text className={style.conservationLabel}>
+            {intl.get('screen.variants.consequences.phyloP17Way')}:
+          </Text>
+          <Text>{conservations.phyloP17way_primate}</Text>
+        </StackLayout>
+      ) : (
+        TABLE_EMPTY_PLACE_HOLDER
+      ),
+  },
+  {
+    key: 'ensembl_transcript_id',
+    title: intl.get('screen.variants.consequences.transcripts'),
+    render: (consequence: IConsequenceEntity) => {
+      const { ensembl_transcript_id, canonical } = consequence;
+      return (
+        <Space>
+          <ExternalLink href={`https://www.ensembl.org/id/${ensembl_transcript_id}`}>
+            {ensembl_transcript_id}
+          </ExternalLink>
+          {canonical && (
+            <Tooltip title={intl.get('screen.variants.consequences.canonical')}>
+              <div>
+                <CanonicalIcon className={style.canonicalIcon} height={14} width={14} />
+              </div>
+            </Tooltip>
+          )}
+        </Space>
+      );
+    },
+  },
+  {
+    title: intl.get('screen.variants.consequences.refSeq'),
+    dataIndex: 'refseq_mrna_id',
+    key: 'consequences',
+    render: (refseq_mrna_id: string[]) => {
+      if (!refseq_mrna_id?.length) return TABLE_EMPTY_PLACE_HOLDER;
+
+      return (
+        <ExpandableCell
+          dataSource={refseq_mrna_id}
+          dictionnary={{
+            'see.less': intl.get('see.less'),
+            'see.more': intl.get('see.more'),
+          }}
+          nOfElementsWhenCollapsed={1}
+          renderItem={(item: string, id) => (
+            <StackLayout horizontal key={id}>
+              <ExternalLink href={`https://www.ncbi.nlm.nih.gov/nuccore/${item}?report=graph`}>
+                {item}
+              </ExternalLink>
+            </StackLayout>
+          )}
+        />
+      );
+    },
+  },
+];
+
+export const expandedRowRender = (row: IGeneEntity): ReactNode => (
+  <Table
+    className={style.expandedTable}
+    columns={getExpandedColumns()}
+    dataSource={hydrateResults(row.consequences?.hits?.edges || [])}
+    pagination={false}
+    bordered
+  />
+);
