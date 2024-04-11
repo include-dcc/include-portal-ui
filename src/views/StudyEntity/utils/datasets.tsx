3c784d625088e6bffbd7fc73aeda25542383852e
GaelleA <galtefrohne@ferlab.bio>
2024-04-11 15:21:17 -0400

    fix(study): SJIP-797 remove empty dataset lines

--- src/views/StudyEntity/utils/datasets.tsx
+++ src/views/StudyEntity/utils/datasets.tsx
@@ -8,57 +8,72 @@ import { IStudyDataset } from 'graphql/studies/models';
 
 import styles from '../index.module.scss';
 
-const getDatasetDescription = (dataset: IStudyDataset): IEntityDescriptionsItem[] => [
-  {
-    label: intl.get('entities.study.dataset.dataset_id'),
-    value: dataset.dataset_id || TABLE_EMPTY_PLACE_HOLDER,
-  },
-  {
-    label: intl.get('entities.study.dataset.data_type'),
-    value: dataset.data_types?.length
-      ? dataset.data_types.map((dataType, index) => <Tag key={index}>{dataType}</Tag>)
-      : TABLE_EMPTY_PLACE_HOLDER,
-  },
-  {
-    label: intl.get('entities.study.dataset.experimental_strategy'),
-    value: dataset.experimental_strategy ? (
-      <Tag>{dataset.experimental_strategy}</Tag>
-    ) : (
-      TABLE_EMPTY_PLACE_HOLDER
-    ),
-  },
-  {
-    label: intl.get('entities.study.dataset.experimental_platform'),
-    value: dataset.experimental_platform || TABLE_EMPTY_PLACE_HOLDER,
-  },
-  {
-    label: intl.get('entities.study.dataset.publication'),
-    value: dataset.publications?.length ? (
-      <ExpandableCell
-        nOfElementsWhenCollapsed={2}
-        dataSource={dataset.publications}
-        renderItem={(sourceText) => (
-          <ExternalLink
-            className={styles.externalLink}
-            href={`https://pubmed.ncbi.nlm.nih.gov/${sourceText.replace('PMID: ', '')}`}
-          >
-            {sourceText}
-          </ExternalLink>
-        )}
-      />
-    ) : (
-      TABLE_EMPTY_PLACE_HOLDER
-    ),
-  },
-  {
-    label: intl.get('entities.study.dataset.repository'),
-    value:
-      dataset.repository && dataset.repository_url ? (
-        <ExternalLink href={dataset.repository_url}>{dataset.repository}</ExternalLink>
+const getDatasetDescription = (dataset: IStudyDataset): IEntityDescriptionsItem[] => {
+  const items: IEntityDescriptionsItem[] = [];
+
+  if (dataset.dataset_id)
+    items.push({
+      label: intl.get('entities.study.dataset.dataset_id'),
+      value: dataset.dataset_id || TABLE_EMPTY_PLACE_HOLDER,
+    });
+
+  if (dataset.data_types?.length)
+    items.push({
+      label: intl.get('entities.study.dataset.data_type'),
+      value: dataset.data_types?.length
+        ? dataset.data_types.map((dataType, index) => <Tag key={index}>{dataType}</Tag>)
+        : TABLE_EMPTY_PLACE_HOLDER,
+    });
+
+  if (dataset.experimental_strategy)
+    items.push({
+      label: intl.get('entities.study.dataset.experimental_strategy'),
+      value: dataset.experimental_strategy ? (
+        <Tag>{dataset.experimental_strategy}</Tag>
       ) : (
         TABLE_EMPTY_PLACE_HOLDER
       ),
-  },
-];
+    });
+
+  if (dataset.experimental_platform)
+    items.push({
+      label: intl.get('entities.study.dataset.experimental_platform'),
+      value: dataset.experimental_platform || TABLE_EMPTY_PLACE_HOLDER,
+    });
+
+  if (dataset.publications?.length)
+    items.push({
+      label: intl.get('entities.study.dataset.publication'),
+      value: dataset.publications?.length ? (
+        <ExpandableCell
+          nOfElementsWhenCollapsed={2}
+          dataSource={dataset.publications}
+          renderItem={(sourceText) => (
+            <ExternalLink
+              className={styles.externalLink}
+              href={`https://pubmed.ncbi.nlm.nih.gov/${sourceText.replace('PMID: ', '')}`}
+            >
+              {sourceText}
+            </ExternalLink>
+          )}
+        />
+      ) : (
+        TABLE_EMPTY_PLACE_HOLDER
+      ),
+    });
+
+  if (dataset.repository && dataset.repository_url)
+    items.push({
+      label: intl.get('entities.study.dataset.repository'),
+      value:
+        dataset.repository && dataset.repository_url ? (
+          <ExternalLink href={dataset.repository_url}>{dataset.repository}</ExternalLink>
+        ) : (
+          TABLE_EMPTY_PLACE_HOLDER
+        ),
+    });
+
+  return items;
+};
 
 export default getDatasetDescription;
