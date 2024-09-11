52677780e64d013acd3a2217764610071fd0fce5
Karine St-Onge <kstonge@ferlab.bio>
2024-09-11 14:52:16 -0400

    feat: SJIP-977 Add Clinical Data Cypress tests

---
+++ cypress/e2e/Telechargement/ClinicalDataTableauParticipants.cy.ts
@@ -0,0 +1,44 @@
+/// <reference types="cypress"/>
+import '../../support/commands';
+import { getDateTime, oneMinute } from '../../support/utils';
+
+const { strDate } = getDateTime();
+
+beforeEach(() => {
+  cy.removeFilesFromFolder(Cypress.config('downloadsFolder'));
+
+  cy.login();
+  cy.visitDataExploration('participants', '?sharedFilterId=75272e84-9a2d-4e0b-b69e-fb9e5df63762');
+  cy.get('div[role="tabpanel"] [class*="ant-table-row"]').eq(0).find('[type="checkbox"]').check({force: true});
+  cy.get('[class*="Header_ProTableHeader"] button[class*="ant-dropdown-trigger"]').eq(1).click({force: true});
+});
+
+describe('Page Data Exploration (Participants) - Télécharger le clinical data', () => {
+  beforeEach(() => {
+    cy.clickAndIntercept('[data-menu-id*="-clinicalData"]', 'POST', '**/file-manifest', 1);
+    cy.waitUntilFile(oneMinute);
+  });
+
+  it('Valider le nom du fichier', () => {
+    cy.validateFileName(`include_clinicalData_${strDate.slice(0, 4)}${strDate.slice(4, 6)}${strDate.slice(6, 8)}T*.xlsx`);
+  });
+
+  it('Valider le contenu du fichier [SJIP-976]', () => {
+    cy.validateXlsxFileContent('DownloadClinicalData.json');
+  });
+});
+
+describe('Page Data Exploration (Participants) - Télécharger le clinical data (family)', () => {
+  beforeEach(() => {
+    cy.clickAndIntercept('[data-menu-id*="-familyClinicalData"]', 'POST', '**/file-manifest', 1);
+    cy.waitUntilFile(oneMinute);
+  });
+
+  it('Valider le nom du fichier', () => {
+    cy.validateFileName(`include_familyClinicalData_${strDate.slice(0, 4)}${strDate.slice(4, 6)}${strDate.slice(6, 8)}T*.xlsx`);
+  });
+
+  it('Valider le contenu du fichier [SJIP-976]', () => {
+    cy.validateXlsxFileContent('DownloadClinicalDataFamily.json');
+  });
+});
