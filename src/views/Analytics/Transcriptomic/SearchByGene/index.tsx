30d26407b1267819f040de80651d525a48b2db93
Alek Perron <alek.perron.hsj@ssss.gouv.qc.ca>
2024-09-19 15:45:32 -0400

    feat: SJIP-958 add search by gene

--- src/views/Analytics/Transcriptomic/SearchByGene/index.tsx
+++ src/views/Analytics/Transcriptomic/SearchByGene/index.tsx
@@ -1,21 +1,92 @@
-import { FilterOutlined } from '@ant-design/icons';
-import { Button, Select } from 'antd';
-import GenesUploadIds from 'views/Variants/components/GeneUploadIds';
+import React, { useEffect, useState } from 'react';
+import intl from 'react-intl-universal';
+import { AutoComplete, Input } from 'antd';
+
+import SearchLabel from '../../../../components/uiKit/search/SearchLabel';
+import {
+  TTranscriptomicsDatum,
+  TTranscriptomicsDiffGeneExp,
+} from '../../../../services/api/transcriptomics/models';
 
 import styles from './index.module.css';
 
-const TranscriptomicSearchByGene = () => (
-  <div className={styles.transcriptomicSearchByGene}>
-    <Select className={styles.select} value="search_by_gene">
-      <Select.Option value="search_by_gene">TODO</Select.Option>
-    </Select>
-    <GenesUploadIds
-      handleUpload={(uniqueMatches) => {
-        // TODO: handle upload
-      }}
-    />
-    <Button icon={<FilterOutlined />} />
-  </div>
-);
+type Props = {
+  options?: TTranscriptomicsDiffGeneExp[];
+  selectedOptionsIds: string[];
+  onSelectOptions: (selectedIds: string[]) => void;
+};
+
+const parseOptions = (options?: TTranscriptomicsDiffGeneExp[]): TTranscriptomicsDatum[] =>
+  options?.flatMap((option) => option.data) || [];
+
+const TranscriptomicSearchByGene = ({ options, selectedOptionsIds, onSelectOptions }: Props) => {
+  const [parsedOptions, setParsedOptions] = useState<TTranscriptomicsDatum[]>([]);
+  const [filteredOptions, setFilteredOptions] = useState<TTranscriptomicsDatum[]>([]);
+  const [inputValue, setInputValue] = useState<string>('');
+
+  useEffect(() => {
+    const parsed = parseOptions(options);
+    setParsedOptions(parsed);
+    setFilteredOptions(parsed);
+
+    const selectedOption = parsed.find(
+      (option) => option.ensembl_gene_id === selectedOptionsIds[0],
+    );
+    if (selectedOption) {
+      setInputValue(selectedOption.gene_symbol);
+    } else {
+      setInputValue('');
+    }
+  }, [options, selectedOptionsIds]);
+
+  const filterOptions = (value: string) => {
+    const filtered = value
+      ? parsedOptions.filter((option) =>
+          option.gene_symbol.toLowerCase().includes(value.toLowerCase()),
+        )
+      : parsedOptions;
+    setFilteredOptions(filtered);
+  };
+
+  const handleSelect = (value: string, option: any) => {
+    const selectedOption = parsedOptions.find((opt) => opt.ensembl_gene_id === option.id);
+    if (selectedOption) {
+      setInputValue(selectedOption.gene_symbol);
+    }
+    onSelectOptions([option.id]);
+  };
+
+  const handleChange = (value: string) => {
+    setInputValue(value);
+    filterOptions(value);
+  };
+
+  return (
+    <div className={styles.transcriptomicSearchByGene}>
+      <div style={{ width: '100%' }}>
+        <SearchLabel
+          title={intl.get('global.search.genes.title')}
+          tooltipText={intl.get('global.search.genes.tooltip')}
+        />
+        <AutoComplete
+          allowClear
+          onSearch={filterOptions}
+          onSelect={handleSelect}
+          placeholder={intl.get('global.search.genes.placeholder')}
+          className={styles.select}
+          value={inputValue}
+          onChange={handleChange}
+          options={filteredOptions.map((option) => ({
+            label: option.gene_symbol,
+            value: option.gene_symbol,
+            id: option.ensembl_gene_id,
+          }))}
+        >
+          <Input />
+        </AutoComplete>
+      </div>
+    </div>
+  );
+};
 
 export default TranscriptomicSearchByGene;
