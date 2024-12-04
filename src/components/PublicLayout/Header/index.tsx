1dd7d745bcec89564d0075aaff9c9117f99a8d3f
Gaelle <133775440+AltefrohneGaelle@users.noreply.github.com>
2024-12-04 09:40:02 -0500

    feat(studies): SJIP-1106 add login modal

--- src/components/PublicLayout/Header/index.tsx
+++ src/components/PublicLayout/Header/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import intl from 'react-intl-universal';
 import {
   DotChartOutlined,
@@ -21,6 +22,8 @@ import useQueryParams from 'hooks/useQueryParams';
 import { trackVisitResources } from 'services/analytics';
 import { STATIC_ROUTES } from 'utils/routes';
 
+import LoginModal from '../LoginModal';
+
 import HeaderButton from './HeaderButton';
 
 import style from './index.module.css';
@@ -32,6 +35,9 @@ const { Text } = Typography;
 const Header = () => {
   const { keycloak } = useKeycloak();
   const query = useQueryParams();
+
+  const [openLoginModal, setOpenLoginModal] = useState(false);
+  const closeLoginModal = () => setOpenLoginModal(false);
 
   const handleSignin = async () => {
     const url = keycloak.createLoginUrl({
@@ -47,145 +53,153 @@ const Header = () => {
   const ft_analyticsPage = getFTEnvVarByKey('ANALYTICS_PAGE');
 
   return (
-    <PageHeader
-      title={<IncludeIcon className={style.logo} />}
-      subTitle={
-        <nav className={style.headerList}>
-          <HeaderButton
-            key="dashboard"
-            icon={<HomeOutlined />}
-            title={intl.get('layout.main.menu.dashboard')}
-          />
-          <HeaderButton
-            key="studies"
-            icon={<ReadOutlined />}
-            isActive={true}
-            title={intl.get('layout.main.menu.studies')}
-          />
-          <HeaderButton
-            key="explore-data"
-            icon={<FileSearchOutlined />}
-            title={intl.get('layout.main.menu.explore')}
-          />
+    <>
+      <PageHeader
+        title={<IncludeIcon className={style.logo} />}
+        subTitle={
+          <nav className={style.headerList}>
+            <HeaderButton
+              key="dashboard"
+              icon={<HomeOutlined />}
+              onClick={() => setOpenLoginModal(true)}
+              title={intl.get('layout.main.menu.dashboard')}
+            />
+            <HeaderButton
+              key="studies"
+              icon={<ReadOutlined />}
+              isActive={true}
+              title={intl.get('layout.main.menu.studies')}
+            />
+            <HeaderButton
+              key="explore-data"
+              icon={<FileSearchOutlined />}
+              onClick={() => setOpenLoginModal(true)}
+              title={intl.get('layout.main.menu.explore')}
+            />
 
-          {ft_variant === 'true' && (
-            <HeaderButton
-              key="variant-data"
-              icon={<LineStyleIcon />}
-              title={intl.get('layout.main.menu.variants')}
-            />
-          )}
-          {ft_analyticsPage === 'true' && (
-            <HeaderButton
-              key="analytics-data"
-              icon={<DotChartOutlined />}
-              title={intl.get('layout.main.menu.analysis')}
-            />
-          )}
-        </nav>
-      }
-      extra={[
-        <HeaderButton
-          key="community"
-          icon={<TeamOutlined />}
-          title={intl.get('layout.main.menu.community')}
-        />,
-        <Dropdown
-          overlayClassName={style.dropdown}
-          key="resources"
-          trigger={['click']}
-          menu={{
-            items: [
-              {
-                key: 'website',
-                disabled: false,
-                label: (
-                  <a
-                    href="#"
-                    onClick={() => {
-                      trackVisitResources('website');
-                      window.open('https://includedcc.org/', '_blank');
-                    }}
-                  >
-                    <ExternalLinkIcon {...iconSize} />
-                    <Text className={style.linkText}>{intl.get('layout.main.menu.website')}</Text>
-                  </a>
-                ),
-              },
-              {
-                key: 'help',
-                label: (
-                  <a
-                    href="#"
-                    onClick={() => {
-                      trackVisitResources('help');
-                      window.open('https://help.includedcc.org/docs/quick-start-guide', '_blank');
-                    }}
-                  >
-                    <ExternalLinkIcon {...iconSize} />
-                    <Text className={style.linkText}>{intl.get('layout.main.menu.help')}</Text>
-                  </a>
-                ),
-              },
-              {
-                key: 'forum',
-                label: (
-                  <a
-                    href="#"
-                    onClick={() => {
-                      trackVisitResources('forum');
-                      window.open('https://help.includedcc.org/discuss', '_blank');
-                    }}
-                  >
-                    <ExternalLinkIcon {...iconSize} />
-                    <Text className={style.linkText}>{intl.get('layout.main.menu.forum')}</Text>
-                  </a>
-                ),
-              },
-              {
-                type: 'divider',
-              },
-              {
-                key: 'contact',
-                label: (
-                  <a
-                    href="#"
-                    onClick={() => {
-                      trackVisitResources('contact');
-                      window.open(
-                        'https://app.smartsheet.com/b/form/514745159a004c2e987fff0aa16ceaac',
-                        '_blank',
-                      );
-                    }}
-                  >
-                    <MailOutlined />
-                    <Text className={style.linkText}>{intl.get('layout.main.menu.contact')}</Text>
-                  </a>
-                ),
-              },
-            ],
-          }}
-        >
-          <a className={style.resourcesMenuTrigger} onClick={(e) => e.preventDefault()} href="">
-            <span className={style.resources}>{intl.get('layout.main.menu.resources')}</span>
-            <DownOutlined />
-          </a>
-        </Dropdown>,
-        <div className={style.connectionWrapper}>
+            {ft_variant === 'true' && (
+              <HeaderButton
+                key="variant-data"
+                icon={<LineStyleIcon />}
+                onClick={() => setOpenLoginModal(true)}
+                title={intl.get('layout.main.menu.variants')}
+              />
+            )}
+            {ft_analyticsPage === 'true' && (
+              <HeaderButton
+                key="analytics-data"
+                icon={<DotChartOutlined />}
+                onClick={() => setOpenLoginModal(true)}
+                title={intl.get('layout.main.menu.analysis')}
+              />
+            )}
+          </nav>
+        }
+        extra={[
           <HeaderButton
-            className={style.loginBtn}
             key="community"
-            icon={<LoginOutlined />}
-            onClick={handleSignin}
-            title={intl.get('screen.loginPage.login')}
-          />
-          <Button className={style.signUpBtn} onClick={handleSignin} type="primary">
-            {intl.get('screen.loginPage.signup')}
-          </Button>
-        </div>,
-      ]}
-      className={style.mainHeader}
-    />
+            icon={<TeamOutlined />}
+            onClick={() => setOpenLoginModal(true)}
+            title={intl.get('layout.main.menu.community')}
+          />,
+          <Dropdown
+            overlayClassName={style.dropdown}
+            key="resources"
+            trigger={['click']}
+            menu={{
+              items: [
+                {
+                  key: 'website',
+                  disabled: false,
+                  label: (
+                    <a
+                      href="#"
+                      onClick={() => {
+                        trackVisitResources('website');
+                        window.open('https://includedcc.org/', '_blank');
+                      }}
+                    >
+                      <ExternalLinkIcon {...iconSize} />
+                      <Text className={style.linkText}>{intl.get('layout.main.menu.website')}</Text>
+                    </a>
+                  ),
+                },
+                {
+                  key: 'help',
+                  label: (
+                    <a
+                      href="#"
+                      onClick={() => {
+                        trackVisitResources('help');
+                        window.open('https://help.includedcc.org/docs/quick-start-guide', '_blank');
+                      }}
+                    >
+                      <ExternalLinkIcon {...iconSize} />
+                      <Text className={style.linkText}>{intl.get('layout.main.menu.help')}</Text>
+                    </a>
+                  ),
+                },
+                {
+                  key: 'forum',
+                  label: (
+                    <a
+                      href="#"
+                      onClick={() => {
+                        trackVisitResources('forum');
+                        window.open('https://help.includedcc.org/discuss', '_blank');
+                      }}
+                    >
+                      <ExternalLinkIcon {...iconSize} />
+                      <Text className={style.linkText}>{intl.get('layout.main.menu.forum')}</Text>
+                    </a>
+                  ),
+                },
+                {
+                  type: 'divider',
+                },
+                {
+                  key: 'contact',
+                  label: (
+                    <a
+                      href="#"
+                      onClick={() => {
+                        trackVisitResources('contact');
+                        window.open(
+                          'https://app.smartsheet.com/b/form/514745159a004c2e987fff0aa16ceaac',
+                          '_blank',
+                        );
+                      }}
+                    >
+                      <MailOutlined />
+                      <Text className={style.linkText}>{intl.get('layout.main.menu.contact')}</Text>
+                    </a>
+                  ),
+                },
+              ],
+            }}
+          >
+            <a className={style.resourcesMenuTrigger} onClick={(e) => e.preventDefault()} href="">
+              <span className={style.resources}>{intl.get('layout.main.menu.resources')}</span>
+              <DownOutlined />
+            </a>
+          </Dropdown>,
+          <div className={style.connectionWrapper}>
+            <HeaderButton
+              className={style.loginBtn}
+              key="community"
+              icon={<LoginOutlined />}
+              onClick={handleSignin}
+              title={intl.get('screen.loginPage.login')}
+            />
+            <Button className={style.signUpBtn} onClick={handleSignin} type="primary">
+              {intl.get('screen.loginPage.signup')}
+            </Button>
+          </div>,
+        ]}
+        className={style.mainHeader}
+      />
+      {openLoginModal && <LoginModal isOpen={openLoginModal} onClose={closeLoginModal} />}
+    </>
   );
 };
 
---
+++ src/components/PublicLayout/LoginModal/index.tsx
@@ -0,0 +1,63 @@
+import intl from 'react-intl-universal';
+import { useKeycloak } from '@react-keycloak/web';
+import { Button, Modal, Space, Typography } from 'antd';
+
+import { REDIRECT_URI_KEY } from 'common/constants';
+import IncludeIcon from 'components/Icons/IncludeIcon';
+import useQueryParams from 'hooks/useQueryParams';
+import { STATIC_ROUTES } from 'utils/routes';
+
+import style from './index.module.css';
+
+const { Title } = Typography;
+
+type LoginModalProps = {
+  isOpen: boolean;
+  onClose: () => void;
+};
+
+const LoginModal = ({ isOpen, onClose }: LoginModalProps) => {
+  const { keycloak } = useKeycloak();
+  const query = useQueryParams();
+
+  const handleSignin = async () => {
+    const url = keycloak.createLoginUrl({
+      redirectUri: `${window.location.origin}/${
+        query.get(REDIRECT_URI_KEY) || STATIC_ROUTES.STUDIES
+      }`,
+      locale: intl.getInitOptions().currentLocale,
+    });
+    window.location.assign(url);
+  };
+
+  return (
+    <Modal closable open={isOpen} onCancel={onClose} footer={null} className={style.modal}>
+      <div className={style.contentWrapper}>
+        <div className={style.titleWrapper}>
+          <IncludeIcon height={56} />
+          <Title className={style.title} level={2}>
+            {intl.get('screen.publicStudies.loginModal.title')}
+          </Title>
+        </div>
+        <div className={style.subtitle}>
+          {intl.get('screen.publicStudies.loginModal.subtitleStart')}
+          <span className={style.subtitleBlue}>
+            {intl.get('screen.publicStudies.loginModal.subtitleBlue')}
+          </span>
+          {intl.get('screen.publicStudies.loginModal.subtitleEnd')}
+        </div>
+        <div className={style.description}>{intl.get('screen.publicStudies.loginModal.text')}</div>
+        <Space size={8}>
+          <Button type="primary" size="large" data-cy="Login" onClick={handleSignin}>
+            {intl.get('screen.publicStudies.loginModal.login')}
+          </Button>
+          <Button ghost size="large" data-cy="Signup" onClick={handleSignin}>
+            {intl.get('screen.publicStudies.loginModal.signup')}
+          </Button>
+        </Space>
+      </div>
+    </Modal>
+  );
+};
+
+export default LoginModal;
