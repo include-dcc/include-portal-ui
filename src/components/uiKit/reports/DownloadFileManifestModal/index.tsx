e9db63549da4e07110d5edfec8d2343bf68ac497
Gaelle <133775440+AltefrohneGaelle@users.noreply.github.com>
2025-01-09 07:58:07 -0500

    feat(study): SJIP-1004 add manifest btn in dataset section (#822)

--- src/components/uiKit/reports/DownloadFileManifestModal/index.tsx
+++ src/components/uiKit/reports/DownloadFileManifestModal/index.tsx
@@ -3,7 +3,8 @@ import intl from 'react-intl-universal';
 import { useDispatch } from 'react-redux';
 import { DownloadOutlined } from '@ant-design/icons';
 import { ISyntheticSqon } from '@ferlab/ui/core/data/sqon/types';
-import { Button, Checkbox, Modal, Tooltip } from 'antd';
+import { Button, Modal, Tooltip } from 'antd';
+import { SizeType } from 'antd/lib/config-provider/SizeContext';
 
 import { ReportType } from 'services/api/reports/models';
 import { fetchReport } from 'store/report/thunks';
@@ -17,22 +18,25 @@ import styles from './index.module.css';
 interface IDownloadFileManifestProps {
   sqon: ISyntheticSqon;
   type?: 'default' | 'primary';
+  size?: SizeType;
   isDisabled?: boolean;
   disabledTooltip?: string;
   hasTooManyFiles?: boolean;
+  fileName?: string;
 }
 
 const DownloadFileManifestModal = ({
   sqon,
   type = 'default',
+  size = undefined,
   isDisabled,
   disabledTooltip,
   hasTooManyFiles,
+  fileName,
 }: IDownloadFileManifestProps) => {
   const dispatch = useDispatch();
 
   const [isModalVisible, setIsModalVisible] = useState(false);
-  const [isFamilyChecked, setIsFamilyChecked] = useState(false);
   let tooltipText = undefined;
   if (isDisabled) {
     if (disabledTooltip) {
@@ -47,7 +51,11 @@ const DownloadFileManifestModal = ({
       <Tooltip title={tooltipText}>
         <Button
           icon={<DownloadOutlined />}
-          onClick={() => setIsModalVisible(true)}
+          onClick={(event) => {
+            event.stopPropagation();
+            setIsModalVisible(true);
+          }}
+          size={size}
           type={type}
           disabled={isDisabled}
         >
@@ -60,15 +68,18 @@ const DownloadFileManifestModal = ({
         okText={intl.get('api.report.fileManifest.okText')}
         okButtonProps={{ disabled: hasTooManyFiles }}
         cancelText={intl.get('api.report.fileManifest.cancel')}
-        onCancel={() => setIsModalVisible(false)}
-        onOk={() => {
+        onCancel={(event) => {
+          event.stopPropagation();
+          setIsModalVisible(false);
+        }}
+        onOk={(event) => {
+          event.stopPropagation();
           dispatch(
             fetchReport({
               data: {
                 name: ReportType.FILE_MANIFEST,
-                fileName: isFamilyChecked ? `familyManifest` : ReportType.FILE_MANIFEST,
+                fileName: fileName || ReportType.FILE_MANIFEST,
                 sqon,
-                withFamily: isFamilyChecked,
               },
               callback: () => setIsModalVisible(false),
             }),
@@ -77,9 +88,6 @@ const DownloadFileManifestModal = ({
         className={styles.modal}
       >
         <p>{intl.getHTML('api.report.fileManifest.text')}</p>
-        <Checkbox checked={isFamilyChecked} onChange={() => setIsFamilyChecked(!isFamilyChecked)}>
-          {intl.get('api.report.fileManifest.textCheckbox')}
-        </Checkbox>
         {hasTooManyFiles && <TooMuchFilesAlert />}
         {!hasTooManyFiles && isModalVisible && <FilesTable sqon={sqon} />}
       </Modal>
