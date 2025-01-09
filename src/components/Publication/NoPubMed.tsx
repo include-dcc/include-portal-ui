9558965e3f3142780fb1654e27b2cefd2e8dd5fe
GaelleA <galtefrohne@ferlab.bio>
2025-01-09 10:50:29 -0500

    fix(study): SJIP-1162 remove copy icon for no pub med

--- src/components/Publication/NoPubMed.tsx
+++ src/components/Publication/NoPubMed.tsx
@@ -1,42 +1,12 @@
 import { ReactElement } from 'react';
-import intl from 'react-intl-universal';
-import { useDispatch } from 'react-redux';
-import { CopyOutlined } from '@ant-design/icons';
 import ExternalLink from '@ferlab/ui/core/components/ExternalLink';
-import { Tooltip } from 'antd';
-import copy from 'copy-to-clipboard';
-
-import { globalActions } from 'store/global';
-
-import style from './index.module.css';
 
 interface NoPubMedProps {
   publication: string;
 }
 
-const NoPubMed = ({ publication }: NoPubMedProps): ReactElement => {
-  const dispatch = useDispatch();
-  return (
-    <>
-      <ExternalLink href={publication}>{publication}</ExternalLink>
-      <Tooltip title={intl.get('entities.study.publicationDetails.copyTooltip')}>
-        <a
-          onClick={() => {
-            copy(publication);
-            dispatch(
-              globalActions.displayMessage({
-                content: intl.get('entities.study.publicationDetails.copyMessage'),
-                type: 'success',
-              }),
-            );
-          }}
-          className={style.copy}
-        >
-          <CopyOutlined />
-        </a>
-      </Tooltip>
-    </>
-  );
-};
+const NoPubMed = ({ publication }: NoPubMedProps): ReactElement => (
+  <ExternalLink href={publication}>{publication}</ExternalLink>
+);
 
 export default NoPubMed;
