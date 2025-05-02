6897b1ddd88f098ddd6ccc265a976b988c048e50
kstonge <karine.st-onge.hsj@ssss.gouv.qc.ca>
2025-05-02 07:29:00 -0400

    feat: SJIP-1352 Adjust Cypress tests related to recent changes

--- cypress/e2e/Telechargement/ClinicalDataPageParticipant_1.cy.ts
+++ cypress/e2e/Telechargement/ClinicalDataPageParticipant_1.cy.ts
@@ -19,7 +19,7 @@ describe('Page d\'un participant - Télécharger le clinical data', () => {
     cy.validateFileName(`include_clinicalData_${strDate.slice(0, 4)}${strDate.slice(4, 6)}${strDate.slice(6, 8)}T*.xlsx`);
   });
 
-  it('Valider le contenu du fichier [SJIP-1233]', () => {
+  it('Valider le contenu du fichier', () => {
     cy.validateXlsxFileContent('DownloadClinicalData.json');
   });
 });
--- cypress/e2e/Telechargement/ClinicalDataPageParticipant_2.cy.ts
+++ cypress/e2e/Telechargement/ClinicalDataPageParticipant_2.cy.ts
@@ -19,7 +19,7 @@ describe('Page d\'un participant - Télécharger le clinical data (family)', () 
     cy.validateFileName(`include_familyClinicalData_${strDate.slice(0, 4)}${strDate.slice(4, 6)}${strDate.slice(6, 8)}T*.xlsx`);
   });
 
-  it('Valider le contenu du fichier [SJIP-1233]', () => {
+  it('Valider le contenu du fichier', () => {
     cy.validateXlsxFileContent('DownloadClinicalDataFamily.json');
   });
 });
