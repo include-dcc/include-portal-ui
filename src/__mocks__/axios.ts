9e41518d68915b777c2a0056bfb4aa9d5e4c73c2
GaelleA <galtefrohne@ferlab.bio>
2026-01-12 14:30:39 -0500

    feat(dep): SJIP-1504 fix tests

---
+++ src/__mocks__/axios.ts
@@ -0,0 +1,24 @@
+const mockAxiosInstance = {
+  get: jest.fn(),
+  post: jest.fn(),
+  put: jest.fn(),
+  delete: jest.fn(),
+  patch: jest.fn(),
+  interceptors: {
+    request: { use: jest.fn(), eject: jest.fn() },
+    response: { use: jest.fn(), eject: jest.fn() },
+  },
+};
+
+export default {
+  create: jest.fn(() => mockAxiosInstance),
+  get: jest.fn(),
+  post: jest.fn(),
+  put: jest.fn(),
+  delete: jest.fn(),
+  patch: jest.fn(),
+  interceptors: {
+    request: { use: jest.fn(), eject: jest.fn() },
+    response: { use: jest.fn(), eject: jest.fn() },
+  },
+};
